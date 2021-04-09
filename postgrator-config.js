9d73a7a1412e938e1dcc5e8d8cf22ed621f5c223
nikki-ai <shony1198@gmail.com>
2021-04-09 16:47:41 -0500

    changed package json

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,10 +8,6 @@ pg.defaults.ssl =
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  // host: process.env.MIGRATION_DB_HOST,
-  // port: process.env.MIGRATION_DB_PORT,
-  // database: process.env.MIGRATION_DB_NAME,
-  // username: process.env.MIGRATION_DB_USER,
-  // password: process.env.MIGRATION_DB_PASS,
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production',
 };
