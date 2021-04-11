6e00f43b21f42d74dbde77309f38d4ba426f3ea9
nikki-ai <shony1198@gmail.com>
2021-04-11 13:39:08 -0500

    changed morganOption

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const sodInstallRouter = require('./services/sodInstall-router');
 
 const app = express();
 
-const morganOption = NODE_ENV === 'production';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
